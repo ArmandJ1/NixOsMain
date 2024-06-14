60b04f2eae10e512c61c9b79fbea46a3ded394ee
Armand Juston <146789835+ArmandJ1@users.noreply.github.com>
2024-06-14 16:51:11 -0400

    Update cli.nix

--- modules/nixos/cli.nix
+++ modules/nixos/cli.nix
@@ -12,9 +12,34 @@
 			file
 		];
 	};
+	# Configure eza
+	programs.eza = {
+		enable = true;
+		icons = true;
+		extraOptions = [
+			"--group-directories-first"
+			"--header"
+			"--tree"
+		];
+	};
+	# Configure fzf
+	programs.fzf = {
+		enable = true;
+	  	defaultOptions = [
+			"--preview 'cat {}'"
+		];	
+	};
 	# Configure zellij
 	programs.zellij = {
 		enable = true;
 		enableZshIntegration = true;
+	};
+	# Configure zsh
+	programs.zsh = {
+		enable = true;
+		autosuggestion.enable = true;
+		autocd = true;
+		history.ignoreDups = true;
+		historySubstringSearch.enable = true;
 	};
 }
