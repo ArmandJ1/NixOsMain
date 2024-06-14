7100bfbeaf1884ee4d2290378a4c48abb1111bc3
Armand Juston <146789835+ArmandJ1@users.noreply.github.com>
2024-06-14 16:30:20 -0400

    Update hypr.nix

--- modules/nixos/hypr.nix
+++ modules/nixos/hypr.nix
@@ -2,18 +2,18 @@
   pkgs,...
 }:
 {
-wayland.windowManager.hyprland = {
-	enable = true;
-	settings = {
-		"$mod" = "SUPER";
-		"$terminal" = "alacritty";
-		"$menu" = "fuzzel";
-		bind = [
-			"$mod, S, exec, shotman --capture region"
-			#"$mod, Return, exec, alacritty"
-			"$mod, D, exec, fuzzel"
-			"$mod, Z, killall"
-		];
+	wayland.windowManager.hyprland = {
+		enable = true;
+		settings = {
+			"$mod" = "SUPER";
+			"$terminal" = "alacritty";
+			"$menu" = "fuzzel";
+			bind = [
+				"$mod, S, exec, shotman --capture region"
+				#"$mod, Return, exec, alacritty"
+				"$mod, D, exec, fuzzel"
+				"$mod, Z, killall"
+			];
+		};
 	};
-  };
 }
